639337975f21825dbb83bb4346f6ee371cdf5cab
guangqiang.chen <guangqiang.chen@tcl.com>
2019-02-01 16:04:01 +0800

    添加执行文件签名
    Signed-off-by: guangqiang.chen <guangqiang.chen@tcl.com>

---
+++ app/signFile/runingo_sign.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#str="./../script"
+#appPath="."
+#TctCamera_M_Global_v0.4.3.1.0002.0_signed_platform_alldpi.apk
+
+str=$1
+appPath=$2
+moduleName=$3
+versionName=$4
+echo "versionName : $versionName"
+
+gradle assembleRelease
+
+#mv ./build/outputs/apk/release/${moduleName}-release-unsigned.apk ./build/outputs/apk/release/${moduleName}-release-unsigned.apk
+
+cp -R /home/user/Android/sign/sign_lib /local/work/Runingo/app/signFile/
+java -Xmx512m -Djava.library.path=${str}/sign_lib -jar ${str}/sign_lib/signapk.jar --min-sdk-version 8 ~/Android/sign/security/platform.x509.pem ~/Android/sign/security/platform.pk8 ${appPath}/build/outputs/apk/release/${moduleName}-release-unsigned.apk  ${appPath}/build/outputs/apk/release/${versionName}.apk
+rm /local/work/Runingo/app/signFile/sign_lib/ -rf
+
+#remove temp apk
+rm ${appPath}/build/outputs/apk/release/${moduleName}-release-unsigned.apk
+
+adb uninstall com.factory.runintestii
+
+adb install -r ${appPath}/build/outputs/apk/release/${versionName}.apk
+
+adb shell am start -n com.factory.runintestii/.activity.MainActivity
--- app/src/main/java/com/factory/runintestii/activity/MainActivity.java
+++ app/src/main/java/com/factory/runintestii/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.factory.runintestii;
+package com.factory.runintestii.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.factory.runintestii.R;
 
 public class MainActivity extends AppCompatActivity {
 
